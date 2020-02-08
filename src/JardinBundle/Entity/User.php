92890da0df9f825455b0ac3f855b1ec8c9ea3523
ayoub <wwzera05@gmail.com>
2020-02-08 23:17:04 +0100

    Initial commit

---
+++ src/JardinBundle/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace JardinBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="JardinBundle\Repository\UserRepository")
+ */
+class User extends  BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
